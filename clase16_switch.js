9e49503928f748d3c754043951498b8a792bb68a
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2020-07-06 12:04:10 -0400

    Finished lect19

---
+++ clase16_switch.js
@@ -0,0 +1,17 @@
+var signo = prompt('Cual es tu signo?')
+
+switch (signo) {
+  case 'acuario':
+    console.log('Acuario: BSDFASDFA asfdas asdfads fasdfasdf asfas fasfa asdfa asdfa ')
+    break
+  case 'piscis':
+  case 'piiscis':
+    console.log('Piscis: asdasdf asdfa sasdfa asdfa asdf asd;jal;k a adfa s')
+    break
+  case 'cancer':
+    console.log('Cancer: asdasdf asdfa sasdfa asdfa asdf asd;jal;k a adfa s')
+    break
+  default:
+    console.log('No es un signo zodiacal valido')
+    break
+}
