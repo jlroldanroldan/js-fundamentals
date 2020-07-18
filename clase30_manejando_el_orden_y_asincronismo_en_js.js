1cb1caf7b8835fc399dc05a2978fee1658bba30d
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2020-07-18 09:22:31 -0400

    finished lect37

--- clase30_manejando_el_orden_y_asincronismo_en_js.js
+++ clase30_manejando_el_orden_y_asincronismo_en_js.js
@@ -1,32 +1,40 @@
-const API_URL = 'https://swapi.dev/api/'
-const PEOPLE_URL = 'people/:id'
+const API_URL = "https://swapi.dev/api/";
+const PEOPLE_URL = "people/:id";
 
-const opts = { crossDomain: true}
+const opts = { crossDomain: true };
 
-function obtenerPersonaje(id,callback){
-  const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
+function obtenerPersonaje(id, callback) {
+  const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`;
 
-  $.get(url, opts, function(persona){
-    console.log(`Hola, yo soy, ${persona.name}`)
-
-    if (callback){
-      callback()
-    }
-  })
+  $.get(url, opts, callback).fail(() => {
+    console.log(`Sucedio un error. No se pudo obtener el personaje ${id}`);
+  });
 }
 
-obtenerPersonaje(1, function(){
-  obtenerPersonaje(2, function(){
-   obtenerPersonaje(3, function(){
-     obtenerPersonaje(4, function(){
-       obtenerPersonaje(5, function(){
-         obtenerPersonaje(6, function(){
-           obtenerPersonaje(7, function(){
+obtenerPersonaje(1, function (persona) {
+  console.log(`Hola, yo soy, ${persona.name}`);
 
-           })
-         })
-       })
-     })
-   })
-  })
-})
+  obtenerPersonaje(2, function (persona) {
+    console.log(`Hola, yo soy, ${persona.name}`);
+
+    obtenerPersonaje(3, function (persona) {
+      console.log(`Hola, yo soy, ${persona.name}`);
+
+      obtenerPersonaje(4, function (persona) {
+        console.log(`Hola, yo soy, ${persona.name}`);
+
+        obtenerPersonaje(5, function (persona) {
+          console.log(`Hola, yo soy, ${persona.name}`);
+
+          obtenerPersonaje(6, function (persona) {
+            console.log(`Hola, yo soy, ${persona.name}`);
+
+            obtenerPersonaje(7, function (persona) {
+              console.log(`Hola, yo soy, ${persona.name}`);
+            });
+          });
+        });
+      });
+    });
+  });
+});
